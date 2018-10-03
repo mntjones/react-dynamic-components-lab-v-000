676a1c945e94570e329f477b269a715e347e8341
Monica Tamaru Jones <mntamaru@gmail.com>
2018-10-03 16:30:29 +0000

    automatically backed up by learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,10 +1,10 @@
 // add Comment component here
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export default class Comment extends Component {
 
   render() {
-    return <div className = "comment">{ this.props.commentText }</div>;
+    return (<div className = "comment">{ this.props.commentText }</div>)
   }
 
 }
